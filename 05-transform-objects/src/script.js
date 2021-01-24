6c763e52d6d5e1bdc6771349c24d3da65436e109
Jesse <63426722+jsealt@users.noreply.github.com>
2021-01-24 18:18:00 -0500

    Create group of boxes
    Created group, modified group as whole using same methods.

--- 05-transform-objects/src/script.js
+++ 05-transform-objects/src/script.js
@@ -11,33 +11,35 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x623cea })
-const mesh = new THREE.Mesh(geometry, material)
-//Add mesh to scene
-scene.add(mesh)
+//group
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
 
-//Position
-// mesh.position.x = 0.7
-// mesh.position.y = - 0.6
-// mesh.position.z = 1
-mesh.position.set(0.7, -0.6, 1)
+//Cube1
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xbf1363})
+)
+group.add(cube1)
 
-//Scale
-// mesh.scale.x = 2
-// mesh.scale.y = 0.5
-// mesh.scale.z = 0.5
-mesh.scale.set(2, 0.5, 0.5)
+//Cube2
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0x623CEA})
+)
+cube2.position.x = -2
+group.add(cube2)
 
-//Rotation
-//Rotation uses Euler (like putting a stick through the center of the axis)
-//reorder lets you reorder your axis. Axis order can skew how the camera turns and twists. in a FPS you want Y, then X. 
-mesh.rotation.reorder('YXZ')
-mesh.rotation.x = Math.PI * 0.25
-mesh.rotation.y = Math.PI * 0.25
-
-//Rotation with Quaternion
-
+//Cube3
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1,1,1),
+    new THREE.MeshBasicMaterial({color: 0xfcfcfc})
+)
+cube3.position.x = 2
+group.add(cube3)
 
 // Axes helper
 const axesHelper = new THREE.AxesHelper(2)
@@ -59,12 +61,6 @@ camera.position.z = 3
 // camera.position.y = 1
 // camera.position.x = 1
 scene.add(camera)
-
-//Camera Look-at method
-//create new point to look at
-// camera.lookAt(new THREE.Vector3(3, 0, 0))
-//Look at object specifically
-camera.lookAt(mesh.position)
 
 
 /**
