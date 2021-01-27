867c8dd30325d40210f7623dc6a38bc7ce83f7a3
Jesse <63426722+jsealt@users.noreply.github.com>
2021-01-26 19:57:23 -0500

    Revert "Revert "added y back in and multiplied by 5""
    This reverts commit 34015b41ef28c8390de97a87a49854ce2c919ad7.

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -74,6 +74,7 @@ const tick = () =>
     //Update camera (on each tick)
     camera.position.x = Math.sin(cursor.x * Math.PI*2) * 3
     camera.position.z = Math.cos(cursor.x * Math.PI*2) * 3
+    camera.position.y = cursor.y * 5
     camera.lookAt(mesh.position)
 
 
