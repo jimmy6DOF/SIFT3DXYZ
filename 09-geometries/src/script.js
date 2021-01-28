42dcb5c675a8e2234e0997023f59fce59cd2e631
Jesse <63426722+jsealt@users.noreply.github.com>
2021-01-28 18:35:35 -0500

    added custom geometry
    // const positionArray = new Float32Array(9)
    // const positionArray = new Float32Array([......
    
    The first is for if you want to come back to it later. You're saying its 9 vertices. 3 sets of coordinates vertexes. xyz.
    
    The second is when you have all coordinates and want to pass them in at once. Formatted this way to highlight that there are three vertices with x,y,z coordinates. (Think of a triangle)

--- 09-geometries/src/script.js
+++ 09-geometries/src/script.js
@@ -12,8 +12,36 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x623cea })
+// const geometry = new THREE.BoxBufferGeometry(1, 1, 1, 3, 2, 2)
+
+// const positionArray = new Float32Array(9)
+
+// // first vertex x y z
+// positionArray[0] = 0
+// positionArray[1] = 0
+// positionArray[2] = 0
+
+// // second vertex x y z
+// positionArray[3] = 0
+// positionArray[4] = 1
+// positionArray[5] = 0
+
+// // third vertex x y z
+// positionArray[6] = 1
+// positionArray[7] = 0
+// positionArray[8] = 0
+
+const positionArray = new Float32Array([
+    0,0,0,
+    0,1,0,
+    1,0,0
+])
+
+
+const material = new THREE.MeshBasicMaterial({ 
+    color: 0x623cea,
+    wireframe: true
+})
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
