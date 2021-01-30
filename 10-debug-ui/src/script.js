3c49ec14c5d9cb15797a6f32caef8678ec3684ea
Jesse <63426722+jsealt@users.noreply.github.com>
2021-01-29 19:40:33 -0500

    replace color of material with param
    this will ensure that the color in the debug  ui matches the color of the cube upon initial load

--- 10-debug-ui/src/script.js
+++ 10-debug-ui/src/script.js
@@ -28,7 +28,7 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2)
-const material = new THREE.MeshBasicMaterial({ color: 0x623cea })
+const material = new THREE.MeshBasicMaterial({ color: parameters.color })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
