95f1987cf34fb53cfc3dbdb768358dc3119cab4e
Jesse <63426722+jsealt@users.noreply.github.com>
2021-01-31 14:26:44 -0500

    replace color with material using map
    inside of the the material replace color: 0xffffff with map: texture
    
    p.s. careful with spacing. "map :" won't work. needs to be "map:"

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -27,7 +27,7 @@ const scene = new THREE.Scene()
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x623cea })
+const material = new THREE.MeshBasicMaterial({ map: texture })
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
