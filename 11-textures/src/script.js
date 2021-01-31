40fd9a6b57a03b71c9265c44233ce7277e6ca32c
Jesse <63426722+jsealt@users.noreply.github.com>
2021-01-31 14:23:08 -0500

    Update script.js
    forgot to include this with the last

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -5,10 +5,11 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 //Textures
 
 const image = new Image()
+const texture = new THREE.Texture(image)
+
 image.onload = () =>
 {
-    const texture = new THREE.Texture(image)
-    console.log(texture)
+    texture.needsUpdate = true
 }
 image.src = '/textures/door/color.jpg'
 
