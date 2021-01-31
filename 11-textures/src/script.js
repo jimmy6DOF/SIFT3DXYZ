26d2bb118e19cebc0912b7ccb2d302006a273b59
Jesse <63426722+jsealt@users.noreply.github.com>
2021-01-31 17:11:01 -0500

    magnification with mag filter
    The magnification filter works just like the minification filter, but when the pixels of the texture are bigger than the render's pixels. In other words, the texture too small for the surface it covers.
    
    The texture gets all blurry because it's a very small texture on a very large surface.
    
    While you might think this looks awful, it is probably for the best. If the effect isn't too exaggerated, the user will probably not even notice it.
    
    You can change the magnification filter of the texture using the magFilter property.
    
    There are only two possible values:
    
    THREE.NearestFilter
    THREE.LinearFilter
    The default is THREE.LinearFilter.
    
    If you test the THREE.NearestFilter, you'll see that the base image is preserved, and you get a pixelated texture:
    
    makes it crisp

--- 11-textures/src/script.js
+++ 11-textures/src/script.js
@@ -43,8 +43,7 @@ const roughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 // colorTexture.center.x = 0.5
 // colorTexture.center.y = 0.5
 
-colorTexture.minFilter = THREE.NearestFilter
-
+colorTexture.magFilter = THREE.NearestFilter
 
 /**
  * Base
