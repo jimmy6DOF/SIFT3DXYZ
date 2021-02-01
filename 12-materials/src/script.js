304aba4177564dd6f6e952ae45e1d5e0f46182e2
jsealt <jsealt@users.noreply.github.com>
2021-02-01 18:39:06 -0500

    load all textures

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -2,12 +2,21 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
-//Textures
+/**
+ * Textures
+ */
 const textureLoader = new THREE.TextureLoader()
 
-const doorColorTexture = new textureLoader.load('/textures/door/color.jpg')
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+const matcapTexture = textureLoader.load('/textures/matcaps/1.png')
+const gradientTexture = textureLoader.load('/textures/gradients/3.jpg')
 
-console.log(doorColorTexture)
 /**
  * Base
  */
