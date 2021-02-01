71b0ddb0eeb795f66b92c9f81a7c31d751e5cb22
jsealt <jsealt@users.noreply.github.com>
2021-02-01 18:27:04 -0500

    we can modify all geom materials at once this way

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -14,7 +14,7 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const material = new THREE.MeshBasicMaterial()
+const material = new THREE.MeshBasicMaterial({ color: 0x623cea})
 
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 16, 16),
