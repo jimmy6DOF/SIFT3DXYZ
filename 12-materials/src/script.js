77100b573b1c4e47b6a7ed437de30ed335d7ba5b
jsealt <jsealt@users.noreply.github.com>
2021-02-01 19:14:22 -0500

    add lights
    added ambient light and a pointed light, and tested on lambert material

--- 12-materials/src/script.js
+++ 12-materials/src/script.js
@@ -45,7 +45,11 @@ const scene = new THREE.Scene()
 // const material = new THREE.MeshMatcapMaterial()
 // material.matcap = matcapTexture
 
-const material = new THREE.MeshDepthMaterial()
+// const material = new THREE.MeshDepthMaterial()
+
+const material = new THREE.MeshLambertMaterial()
+
+
 
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(0.5, 16, 16),
@@ -66,7 +70,17 @@ torus.position.x = 1.5
 
 scene.add(sphere, plane, torus)
 
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
+scene.add(ambientLight)
 
+const pointLight = new THREE.PointLight(0xffffff, 0.5)
+pointLight.position.x = 2
+pointLight.position.y = 3
+pointLight.position.z = 4
+scene.add(pointLight)
 
 /**
  * Sizes
