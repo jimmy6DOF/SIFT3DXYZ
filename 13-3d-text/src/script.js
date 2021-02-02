1b1df891eb109baa61d655ab8a3bd6c386c21ee6
jsealt <jsealt@users.noreply.github.com>
2021-02-02 13:55:42 -0500

    Add Text
    comment cube
    added wireframe after (inside of the textMaterial! not the text geometry)

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -23,22 +23,39 @@ const textureLoader = new THREE.TextureLoader()
 /**
  * Object
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
+// const cube = new THREE.Mesh(
+//     new THREE.BoxGeometry(1, 1, 1),
+//     new THREE.MeshBasicMaterial()
+// )
 
-scene.add(cube)
+// scene.add(cube)
 
-//Font
+//Fonts
 const fontLoader = new THREE.FontLoader()
 fontLoader.load(
     '/fonts/helvetiker_regular.typeface.json',
-    () =>
+    (font) =>
     {
-        console.log('font loaded')
+        const textGeometry = new THREE.TextGeometry(
+            'AngellXR',
+            {
+                font: font,
+                size: 0.5,
+                height: 0.2,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 5
+            }
+        )
+        const textMaterial = new THREE.MeshBasicMaterial({ wireframe: true })
+        const text = new THREE.Mesh(textGeometry, textMaterial)
+        scene.add(text)
     }
 )
+
 
 /**
  * Sizes
