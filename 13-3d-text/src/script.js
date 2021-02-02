5d58bf7efc6c11d1fe4628842426f158e543bfbb
jsealt <jsealt@users.noreply.github.com>
2021-02-02 18:10:48 -0500

    leveraging material across text and object
    we had the same material for the text as for the objects, so we can make a common material for them to share.

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -67,20 +67,19 @@ fontLoader.load(
         //Easier way to center
         textGeometry.center()
 
-        const textMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
-        const text = new THREE.Mesh(textGeometry, textMaterial)
+        const material = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
+        const text = new THREE.Mesh(textGeometry, material)
         scene.add(text)
 
         // //add to debug to toggle wireframe
         // gui.add(textMaterial, 'wireframe')
 
         const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
-        const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
-
+        
         for(let i = 0; i < 100; i++)
         {
-            console.time('donut')
-            const donut = new THREE.Mesh(donutGeometry, donutMaterial)
+            // console.time('donut')
+            const donut = new THREE.Mesh(donutGeometry, material)
             donut.position.x = (Math.random() - 0.5) * 10
             donut.position.y = (Math.random() - 0.5) * 10
             donut.position.z = (Math.random() - 0.5) * 10
@@ -93,7 +92,7 @@ fontLoader.load(
 
             scene.add(donut)
         }
-        console.timeEnd('donut')
+        // console.timeEnd('donut')
     }
 )
 
