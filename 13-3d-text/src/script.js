b50a19435a83c09b8fb3b99b052cdcb5ccf05b49
jsealt <jsealt@users.noreply.github.com>
2021-02-02 18:04:27 -0500

    Optimize
    Using console.time before and after the 100 donuts are made, we can get an estimate of how long it took to load.

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -76,12 +76,23 @@ fontLoader.load(
 
         for(let i = 0; i < 100; i++)
         {
+            console.time('donut')
             const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 20, 45)
             const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: matcapTexture })
             const donut = new THREE.Mesh(donutGeometry, donutMaterial)
+            donut.position.x = (Math.random() - 0.5) * 10
+            donut.position.y = (Math.random() - 0.5) * 10
+            donut.position.z = (Math.random() - 0.5) * 10
+            
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+            
+            const scale = Math.random()
+            donut.scale.set(scale, scale, scale)
+
             scene.add(donut)
         }
-
+        console.timeEnd('donut')
     }
 )
 
