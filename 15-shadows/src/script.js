26426aeabb678d02e2470aeb93780221189412f4
jsealt <jsealt@users.noreply.github.com>
2021-02-04 10:33:43 -0500

    Tell renderer to handle Shadow Maps
    have to turn it "on"

--- 15-shadows/src/script.js
+++ 15-shadows/src/script.js
@@ -102,7 +102,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
+renderer.shadowMap = true
 /**
  * Animate
  */
