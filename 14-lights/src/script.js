11bcdab9639ffca71dd0d3d25cb694739a361914
jsealt <jsealt@users.noreply.github.com>
2021-02-04 00:02:16 -0500

    spot light

--- 14-lights/src/script.js
+++ 14-lights/src/script.js
@@ -38,6 +38,10 @@ rectAreaLight.position.set(- 1.5, 0, 1.5)
 rectAreaLight.lookAt(new THREE.Vector3())
 scene.add(rectAreaLight)
 
+const spotLight = new THREE.SpotLight(0x78ff00, 0.5, 10, Math.PI * 0.1, 0.25, 1)
+spotLight.position.set(0, 2, 3)
+scene.add(spotLight)
+
 //debug UI
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001)
