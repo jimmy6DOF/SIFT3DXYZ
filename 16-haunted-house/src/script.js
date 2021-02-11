740afad598d17e4708be395cbb7d77fb41c44b99
Jesse <63426722+jsealt@users.noreply.github.com>
2021-02-11 18:12:45 -0500

    Ghosts
    For the ghosts, let's keep things simple and do with what we know.
    
    We are going to use simple lights floating around the house and passing through the ground and graves.
    
    Now we can animate them using some mathematics with a lot of trigonometry:

--- 16-haunted-house/src/script.js
+++ 16-haunted-house/src/script.js
@@ -209,7 +209,19 @@ const fog = new THREE.Fog('#262837', 1, 15)
 scene.fog = fog
 
 /**
- * Sizes
+ * Ghosts
+ */
+const ghost1 = new THREE.PointLight('#ff00ff', 2, 3)
+scene.add(ghost1)
+
+const ghost2 = new THREE.PointLight('#00ffff', 2, 3)
+scene.add(ghost2)
+
+const ghost3 = new THREE.PointLight('#ffff00', 2, 3)
+scene.add(ghost3)
+
+/**
+ * Window Sizes
  */
 const sizes = {
     width: window.innerWidth,
@@ -263,6 +275,22 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    // Ghosts
+    const ghost1Angle = elapsedTime * 0.5
+    ghost1.position.x = Math.cos(ghost1Angle) * 4
+    ghost1.position.z = Math.sin(ghost1Angle) * 4
+    ghost1.position.y = Math.sin(elapsedTime * 3)
+
+    const ghost2Angle = - elapsedTime * 0.32
+    ghost2.position.x = Math.cos(ghost2Angle) * 5
+    ghost2.position.z = Math.sin(ghost2Angle) * 5
+    ghost2.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5)
+
+    const ghost3Angle = - elapsedTime * 0.18
+    ghost3.position.x = Math.cos(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.32))
+    ghost3.position.z = Math.sin(ghost3Angle) * (7 + Math.sin(elapsedTime * 0.5))
+    ghost3.position.y = Math.sin(elapsedTime * 4) + Math.sin(elapsedTime * 2.5)
 
     // Update controls
     controls.update()
