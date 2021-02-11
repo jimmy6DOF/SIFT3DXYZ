061d2ea50f4f2ed02e1cec464b42d2a0ed4f979e
Jesse <63426722+jsealt@users.noreply.github.com>
2021-02-11 18:04:57 -0500

    update labels before loading in bricks

--- 16-haunted-house/src/script.js
+++ 16-haunted-house/src/script.js
@@ -18,6 +18,7 @@ const scene = new THREE.Scene()
 /**
  * Textures
  */
+//doors textures
 const textureLoader = new THREE.TextureLoader()
 const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
 const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
@@ -26,6 +27,8 @@ const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
 const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
 const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
 const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
+
+// Bricks textures 
 
 /**
  * House
