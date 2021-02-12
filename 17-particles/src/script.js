94acfe0c545d654c7b93c95841712d3ca3cf649b
Jesse <63426722+jsealt@users.noreply.github.com>
2021-02-12 16:01:54 -0500

    FIX: missing a lot of characters
    the computer was getting caught in a loop because I was missing multiple closing parenthesis'

--- 17-particles/src/script.js
+++ 17-particles/src/script.js
@@ -29,15 +29,12 @@ const count = 500
 
 const positions = new Float32Array(count * 3) // Multiply by 3 because each position is composed of 3 values (x, y, z)
 
-for(let i = 0; i , count * 3; i++) //Multiply these by 3 for the same reason xyz
+for(let i = 0; i < count * 3; i++) // Multiply by 3 for same reason
 {
-    positions[i] = Math.random() //this will place the points randomly in the positive direction of x y and z starting at 0, 0 , 0 (meaning it will go up and to the right and expand)
+    positions[i] = (Math.random())// Math.random() - 0.5 to have a random value between -0.5 and +0.5
 }
 
-particlesGeometry.setAttribute(
-    'position', 
-    new THREE.BufferAttribute(positions, 3) //assign positions const as an attribute, and then pass 3 again for the same reason xyz
-)
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3)) // Create the Three.js BufferAttribute and specify that each information is composed of 3 values
 
 //Material
 const particlesMaterial = new THREE.PointsMaterial({ 
