ea29364174aeeb0e2a7a695a3195d21463f72c33
jsealt <jsealt@users.noreply.github.com>
2021-03-01 23:36:13 -0500

    start
    Introduction
    Physics can be one of the coolest features you can add to a WebGL experience. People enjoy playing with objects, see them collide, collapse, fall and bounce like in my portfolio: https://bruno-simon.com/
    
    There are many ways of adding physics to your project, and it depends on what you want to achieve. You can create your own physics with some mathematics and solutions like Raycaster, but if you wish to get realistic physics with tension, friction, bouncing, constraints, pivots, etc. and all that in 3D space, you better use a library.
    
    Theory
    The idea is simple. We are going to create a physics world. This physics world is purely theoretical, and we cannot see it. But in this world, things fall, collide, rub, slide, etc.
    
    When we create a Three.js mesh, we will also create a version of that mesh inside the physics world. If we make a Box in Three.js, we also create a box in the physics world.
    
    Then, on each frame, before rendering anything, we tell the physics world to update itself; we take the coordinates (position and rotation) of the physics objects and apply them to the corresponding Three.js mesh.
    
    And that's all. What is most difficult is to organize our code in a decent structure. That is a part where paths separate. Each developer will have its habits, and it also depends on what you want to do and how complex the physics can become.
    
    To begin with, we will simply create spheres and boxes.
    
    Libraries
    There are multiple available libraries. First, you must decide if you need a 3D library or a 2D library. While you might think it has to be a 3D library because Three.js is all about 3D, you might be wrong. 2D libraries are usually much more performant, and if you can sum up your experience physics up to 2D collisions, you better use a 2D library.
    
    One example is if you want to create a pool game. The balls can collide and bounce on the walls, but you can project everything on a 2D plane. You can design balls as circles in the physics world, and the walls are simple rectangles. Indeed, you won't be able to do tricks hitting the bottom of the ball so that it can jump over the other balls.
    
    An excellent example of a project done like this is Ouigo Let's play by Merci Michel. They used a 2D physics library because every collision and animation can be represented in a 2D space.
    
    For 3D physics, there are three main libraries:
    
    Ammo.js
    Website: http://schteppe.github.io/ammo.js-demos/
    Git repository: https://github.com/kripken/ammo.js/
    Documentation: No documentation
    Direct JavaScript port of Bullet (a physics engine written in C++)
    A little heavy
    Still updated by a community
    Cannon.js
    Website: https://schteppe.github.io/cannon.js/
    Git repository: https://github.com/schteppe/cannon.js
    Documentation: http://schteppe.github.io/cannon.js/docs/
    Lighter than Ammo.js
    More comfortable to implement than Ammo.js
    Mostly maintained by one developer
    Hasn't been updated for many years
    There is a maintained fork
    Oimo.js
    Website: https://lo-th.github.io/Oimo.js/
    Git repository: https://github.com/lo-th/Oimo.js
    Documentation: http://lo-th.github.io/Oimo.js/docs.html
    Lighter than Ammo.js
    Easier to implement than Ammo.js
    Mostly maintained by one developer
    Hasn't been updated for 2 years
    For 2D physics, there are many libraries, but here's the most popular:
    
    Matter.js
    Website: https://brm.io/matter-js/
    Git repository: https://github.com/liabru/matter-js
    Documentation: https://brm.io/matter-js/docs/
    Mostly maintained by one developer
    Still kind of updated

--- 20-physics/src/script.js
+++ 20-physics/src/script.js
@@ -2,6 +2,8 @@ import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import CANNON from 'cannon'
+
 
 /**
  * Debug
@@ -31,6 +33,12 @@ const environmentMapTexture = cubeTextureLoader.load([
     '/textures/environmentMaps/0/pz.png',
     '/textures/environmentMaps/0/nz.png'
 ])
+
+/**
+ * Physics
+ */
+const world = new CANNON.World()
+world.gravity.set(0, -9.82, 0)
 
 /**
  * Test sphere
