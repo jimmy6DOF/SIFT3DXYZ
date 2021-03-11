d525364e2b762233b2e445629b84eea2084814ce
jsealt <jsealt@users.noreply.github.com>
2021-03-11 16:42:54 -0700

    add randomness to sound
    For even more realism, we can add some randomness to the sound volume:

--- 20-physics/src/script.js
+++ 20-physics/src/script.js
@@ -59,9 +59,10 @@ const hitSound = new Audio('/sounds/hit.mp3')
 const playHitSound = (collision) =>
 {
     const impactStrength = collision.contact.getImpactVelocityAlongNormal()
-
+    // console.log(impactStrength)
     if(impactStrength > 1.5)
     {
+        hitSound.volume = Math.random()
         hitSound.currentTime = 0
         hitSound.play()
     }
