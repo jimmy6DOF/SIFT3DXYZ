dae4ced75ee1987c39d1cf4f7c5e24fbc5edd09e
jsealt <jsealt@users.noreply.github.com>
2021-03-15 22:45:22 -0400

    remove with datgui reset button
    Let's add a reset button.
    
    Create a reset function and add it to your Dat.GUI as we did for createBox and createSphere:

--- 20-physics/src/script.js
+++ 20-physics/src/script.js
@@ -39,8 +39,24 @@ debugObject.createBox = () =>
         }
     )
 }
+
+debugObject.reset = () =>
+{
+    for(const object of objectsToUpdate)
+    {
+        // Remove body
+        object.body.removeEventListener('collide', playHitSound)
+        world.removeBody(object.body)
+
+        // Remove mesh
+        scene.remove(object.mesh)
+    }
+}
+
 //add it to debug
 gui.add(debugObject, 'createBox')
+gui.add(debugObject, 'reset')
+
 
 /**
  * Base
