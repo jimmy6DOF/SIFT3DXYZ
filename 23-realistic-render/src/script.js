cd6832af32b56a46e7b9d437d46540f30323993f
Jesse <63426722+jsealt@users.noreply.github.com>
2021-04-07 10:21:02 -0400

    add dat.GUI parameters for lighting
    We can now control the position and intensity.

--- 23-realistic-render/src/script.js
+++ 23-realistic-render/src/script.js
@@ -31,6 +31,10 @@ const directionalLight = new THREE.DirectionalLight('#fcfcfc', 1)
 directionalLight.position.set(0.25, 3, -2.25)
 scene.add(directionalLight)
 
+gui.add(directionalLight, 'intensity').min(0).max(10).step(0.001).name('lightIntensity')
+gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001).name('lightX')
+gui.add(directionalLight.position, 'y').min(-5).max(5).step(0.001).name('lightY')
+gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001).name('lightZ')
 /**
  * Sizes
  */
