7048a8c2b55a0f21d159241d29e811b76671b196
Jesse <63426722+jsealt@users.noreply.github.com>
2021-04-14 22:31:04 -0400

    plug in shaders as material
    If you log testVertexShader and testFragmentShader, you'll get the shader code as a plain string. We can use these two variables in our RawShaderMaterial:

--- 24-shaders/src/script.js
+++ 24-shaders/src/script.js
@@ -30,27 +30,10 @@ const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
 
 // Material
 const material = new THREE.RawShaderMaterial({
-    vertexShader: `
-        uniform mat4 projectionMatrix;
-        uniform mat4 viewMatrix;
-        uniform mat4 modelMatrix;
-
-        attribute vec3 position;
-
-        void main()
-        {
-            gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
-        }
-    `,
-    fragmentShader: `
-        precision mediump float;
-
-        void main()
-        {
-            gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-        }
-    `
+    vertexShader: testVertexShader,
+    fragmentShader: testFragmentShader
 })
+
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
