545cf234c4cf9bb3a677d09a222acc837f0f7d0d
Jesse <63426722+jsealt@users.noreply.github.com>
2021-04-18 21:21:55 -0400

    Vector 4 (4th value is w -> x,y,z,w)
    Finally, the vec4 works like it's two predecessors but with a fourth value named w or a —w because there is no letter after z in the alphabet and a for "alpha":
    
    There are other types of variables such as mat2, mat3, mat4, or sampler2D, but we will see those later.

--- 24-shaders/src/shaders/test/vertex.glsl
+++ 24-shaders/src/shaders/test/vertex.glsl
@@ -7,20 +7,8 @@ attribute vec3 position;
 
 void main()
 {
-     // vec3 foo = vec3(0.0);
-     // vec3 bar = vec3(1.0, 2.0, 3.0);
-     // bar.z = 4.0;
-
-     // vec3 purpleColor = vec3(0.0);
-     // purpleColor.r = 0.5;
-     // purpleColor.b = 1.0;
-
-     // vec2 foo = vec2(1.0, 2.0);
-     // vec3 bar =vec3(foo, 3.0);
-
-     vec3 foo = vec3(1.0, 2.0, 3.0);
-     vec2 bar = foo.xz;
-
+     vec4 foo = vec4(1.0, 2.0, 3.0, 4.0);
+     vec4 bar = vec4(foo.zw, vec2(5.0, 6.0));
 
      gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
 }
