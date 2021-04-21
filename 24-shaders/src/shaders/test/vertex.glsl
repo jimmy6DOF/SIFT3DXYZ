36bbcd6be5766d8a452b50a2a05bd4b30fc50f73
Jesse <63426722+jsealt@users.noreply.github.com>
2021-04-21 15:52:36 -0400

    Position attributes
    First, we retrieve the vertex position with:
    
    attribute vec3 position;
    GLSL
    Remember that the same code applies to every vertices of the geometry. Attributes are the only variable that will change between the vertices. The same vertex shader will be applied for each vertex and the position attribute will contain the x, y, and z coordinates of that specific vertex.
    
    Then, we convert this vec3 to a vec4:
    
    gl_Position = /* ... */ vec4(position, 1.0);
    GLSL
    This is because the following matrices and the gl_Position need to use vec4 as we saw earlier.

--- 24-shaders/src/shaders/test/vertex.glsl
+++ 24-shaders/src/shaders/test/vertex.glsl
@@ -3,12 +3,11 @@ uniform mat4 projectionMatrix;
 uniform mat4 viewMatrix;
 uniform mat4 modelMatrix;
 
-attribute vec3 position;
+attribute vec3 position; //contains x,y,and z because its vec3
 
 void main()
 {
-     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);//use vec4 because matrices are mat4's
 
-     gl_Position.x += 0.5;
-     gl_Position.y += -0.5;
+
 }
