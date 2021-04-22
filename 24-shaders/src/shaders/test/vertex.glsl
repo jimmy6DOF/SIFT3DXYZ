3bee1f45c1fa8ba84d8d330bbcd718ad1b36e7e7
Jesse <63426722+jsealt@users.noreply.github.com>
2021-04-21 20:05:21 -0400

    now we can make more specific mods to the mode, view, and proejcted positions

--- 24-shaders/src/shaders/test/vertex.glsl
+++ 24-shaders/src/shaders/test/vertex.glsl
@@ -7,7 +7,11 @@ attribute vec3 position; //contains x,y,and z because its vec3
 
 void main()
 {
-     gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);//use vec4 because matrices are mat4's
+     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
+     modelPosition.y += 1.0;
+     vec4 viewPosition = viewMatrix * modelPosition;
+     vec4 projectedPosition = projectionMatrix * viewPosition;
 
+     gl_Position = projectedPosition;
 }
