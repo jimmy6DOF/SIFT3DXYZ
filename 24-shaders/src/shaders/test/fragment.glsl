70d393c5fc101e095bec422af4b10b956541b2ec
jsealt <jsealt@users.noreply.github.com>
2021-04-27 22:59:21 -0400

    revert back to plane 1:35:26

--- 24-shaders/src/shaders/test/fragment.glsl
+++ 24-shaders/src/shaders/test/fragment.glsl
@@ -1,10 +1,10 @@
 precision mediump float;
 
-varying float vRandom;
+// varying float vRandom;
 
 void main()
 {
     // gl_FragColor = vec4(vRandom, vRandom, 1.0, 0.5);
-    gl_FragColor = vec4(0.25, vRandom * 0.45, .5969, 1.0);
+    gl_FragColor = vec4(0.25, 0.15, .5969, 1.0);
 }
 
--- 24-shaders/src/shaders/test/vertex.glsl
+++ 24-shaders/src/shaders/test/vertex.glsl
@@ -6,18 +6,15 @@ uniform mat4 modelMatrix;
 attribute vec3 position; //contains x,y,and z because its vec3
 attribute float aRandom;
 
-varying float vRandom;
+// varying float vRandom;
 
 void main()
 {
      vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-     // modelPosition.z += sin(modelPosition.x * 10.0)* 0.1;
-     modelPosition.z += aRandom * 0.1;
 
      vec4 viewPosition = viewMatrix * modelPosition;
      vec4 projectedPosition = projectionMatrix * viewPosition;
 
      gl_Position = projectedPosition;
 
-     vRandom = aRandom;
 }
