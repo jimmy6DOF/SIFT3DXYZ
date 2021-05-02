54c03d21fe49dc2af76f2ea46bc373742ba0cb5c
jsealt <jsealt@users.noreply.github.com>
2021-05-01 22:42:42 -0400

    tweak

--- 24-shaders/src/shaders/test/fragment.glsl
+++ 24-shaders/src/shaders/test/fragment.glsl
@@ -13,7 +13,7 @@ void main()
     // gl_FragColor = vec4(vRandom, vRandom, 1.0, 0.5);
     // gl_FragColor = vec4(0.25, 0.15, .5969, 1.0);
     vec4 textureColor = texture2D(uTexture, vUv);
-    textureColor.rgb *= vElevation * 1.2;
+    textureColor.rgb *= vElevation + 0.5;
     // gl_FragColor = vec4(uColor, 1.0);
     gl_FragColor = textureColor;
 }
