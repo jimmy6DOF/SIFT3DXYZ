92b7ec75046638164d2e23b918173dcd650472a2
jsealt <jsealt@users.noreply.github.com>
2021-05-10 23:25:57 -0400

    Update fragment.glsl
    using if works but is bad for performance

--- 25-shader-patterns/src/shaders/test/fragment.glsl
+++ 25-shader-patterns/src/shaders/test/fragment.glsl
@@ -5,11 +5,29 @@ void main()
     // Pattern 3
     // float strength = vUv.x;
 
-    // Pattern 4
+    // // Pattern 4
     // float strength = vUv.y;
 
-    // Pattern 5
-    float strength = 1.0 - vUv.y;
+    // // Pattern 5
+    // float strength = 1.0 - vUv.y;
+ 
+    // // Pattern 6
+    // float strength = vUv.y * 10.0;
+ 
+    // // Pattern 7
+    // float strength = mod(vUv.y * 10.0, 1.0);
+ 
+    // Pattern 8
+    float strength = mod(vUv.y * 10.0, 1.0);
+
+    if(strength < 0.5)
+    {
+        strength = 0.0;
+    }
+    else
+    {
+        strength = 1.0;
+    }
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
