6dfcbaaff74525b9f95de68f90badc6aede72045
jsealt <jsealt@users.noreply.github.com>
2021-05-11 23:51:24 -0400

    33:01 save

--- 25-shader-patterns/src/shaders/test/fragment.glsl
+++ 25-shader-patterns/src/shaders/test/fragment.glsl
@@ -33,9 +33,17 @@ void main()
     // float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
     // strength += step(0.8, mod(vUv.y * 10.0, 1.0));
 
-    // Pattern 12
-    float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
-    strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
+    // // Pattern 12
+    // float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
+    // strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
+
+    // // Pattern 13
+    // float strength = step(0.8, mod(vUv.y * 10.0, 1.0));
+    // strength -= step(0.8, mod(vUv.x * 10.0, 1.0));
+
+    // Pattern 14
+    float strength = step(0.8, mod(vUv.y * 10.0, 1.0));
+    strength -= step(0.8, mod(vUv.x * 10.0, 1.0));
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
