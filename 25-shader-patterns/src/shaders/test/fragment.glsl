60f820f4293da68facb1c492152c8e3d87159325
jsealt <jsealt@users.noreply.github.com>
2021-05-23 22:06:08 -0400

    redBluePlane
    Decided to go my own direction (again) and make an interesting plane.

--- 25-shader-patterns/src/shaders/test/fragment.glsl
+++ 25-shader-patterns/src/shaders/test/fragment.glsl
@@ -298,5 +298,5 @@ void main()
     
     float strength = step(0.9,sin(cnoise(vUv * 10.0)* 20.0));
 
-    gl_FragColor = vec4(strength, strength, strength, 1.0);
+    gl_FragColor = vec4(vUv.yx, strength + vUv.x, 1.0);
 }
