003a94a68fd7eb31dc4890c9676d8aa3ff36de3a
jsealt <jsealt@users.noreply.github.com>
2021-05-31 17:48:38 -0400

    this is 16:18
    add comment.

--- 26-raging-sea/src/shaders/water/vertex.glsl
+++ 26-raging-sea/src/shaders/water/vertex.glsl
@@ -3,9 +3,8 @@ uniform float uBigWavesElevation;
 void main()
 {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    
+    // Elevation
     float elevation = sin(modelPosition.x) * uBigWavesElevation;
-
     modelPosition.y += elevation;
 
     vec4 viewPosition = viewMatrix * modelPosition;
