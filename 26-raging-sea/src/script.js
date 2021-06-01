8632da27c9cc9d9a039601f18a2100fb38647f63
jsealt <jsealt@users.noreply.github.com>
2021-06-01 00:17:17 -0400

    animate with uTime

--- 26-raging-sea/src/script.js
+++ 26-raging-sea/src/script.js
@@ -29,6 +29,7 @@ const waterMaterial = new THREE.ShaderMaterial({
     fragmentShader: waterFragmentShader,
     uniforms:
     {
+        uTime: { value: 0 },
         uBigWavesElevation: { value: 0.2 },
         uBigWavesFrequency: { value: new THREE.Vector2(4, 1.5)}
     }
@@ -96,6 +97,8 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    // Update Weter
+    waterMaterial.uniforms.uTime.value = elapsedTime
 
     // Update controls
     controls.update()
