2540e2df686337e8de57d6e42d40ea5c3ff6994f
jsealt <jsealt@users.noreply.github.com>
2021-06-07 23:27:13 -0400

    38:00

--- 26-raging-sea/src/shaders/water/vertex.glsl
+++ 26-raging-sea/src/shaders/water/vertex.glsl
@@ -1,13 +1,14 @@
 uniform float uTime;
 uniform float uBigWavesElevation;
 uniform vec2 uBigWavesFrequency;
+uniform float uBigWavesSpeed;
 
 void main()
 {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
     // Elevation and frequency
-    float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime) *
-                sin(modelPosition.z * uBigWavesFrequency.y + uTime) *
+    float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) *
+                sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) *
                 uBigWavesElevation;
                 
     modelPosition.y += elevation;
