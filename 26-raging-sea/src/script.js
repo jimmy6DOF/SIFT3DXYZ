debd351e724e216dfbcac18f052970460990ce1a
jsealt <jsealt@users.noreply.github.com>
2021-06-12 00:15:39 -0400

    incredible work
    for(float i = 1.0; i <= 3.0; i++)
    {
        elevation -= abs(cnoise(vec3(modelPosition.xz * 3.0, uTime * 0.2)) * 0.15);
    }

--- 26-raging-sea/src/script.js
+++ 26-raging-sea/src/script.js
@@ -67,7 +67,9 @@ gui
     .onChange(() =>
     {
         waterMaterial.uniforms.uSurfaceColor.value.set(debugObject.surfaceColor)
-    })
+    }),
+gui.add(waterMaterial.uniforms.uColorOffset, 'value').min(0).max(1).step(0.001).name('uColorOffset')
+gui.add(waterMaterial.uniforms.uColorMultiplier, 'value').min(0).max(10).step(0.001).name('uColorMultiplier')
 
 // Mesh
 const water = new THREE.Mesh(waterGeometry, waterMaterial)
