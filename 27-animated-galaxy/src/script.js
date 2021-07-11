d520cff8a7e216e93e182c464ad88c8347c98ae4
jsealt <jsealt@users.noreply.github.com>
2021-07-11 14:03:28 -0400

    create our shaders
    Clearly, we need to provide our own shaders. Add the following vertexShader:
    
    The beginning of the shader is the same as we've already seen. We update the position by using successively the modelMatrix, the viewMatrix, and the projectionMatrix. But then, we assign a new variable called gl_PointSize with 2.0 as the value.
    
    gl_PointSize is precisely what you might think. The particles will have a 2x2 size, and you should see 2x2 particles regardless of the distance of the camera.
    
    The unit here are fragments and if you are using a normal screen with a pixel ratio of 1, you'll get 2 pixels by 2 pixels because 1 fragment = 1 pixel. But if you are using a screen with a higher pixel ratio like a retina screen, 1 fragment will be smaller than 1 pixel and you should get smaller particles. We'll fix that later in order to get a consistent result through any pixel ratio.
    
    Before we improve the particles size, let's change the color.
    
    The particles are currently red because we didn't provide any fragmentShader and Three.js uses a default one with a red output.
    
    Add the following fragment shader with a white color:

--- 27-animated-galaxy/src/script.js
+++ 27-animated-galaxy/src/script.js
@@ -88,8 +88,31 @@ const generateGalaxy = () =>
     material = new THREE.ShaderMaterial({
         depthWrite: false,
         blending: THREE.AdditiveBlending,
-        vertexColors: true
-    })
+        vertexColors: true,
+        vertexShader:`
+        void main()
+        {
+            /**
+             * Position
+             */
+            vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+            vec4 viewPosition = viewMatrix * modelPosition;
+            vec4 projectedPosition = projectionMatrix * viewPosition;
+            gl_Position = projectedPosition;
+
+            /**
+             * Size
+             */
+            gl_PointSize = 2.0;
+        }
+    `,
+        fragmentShader: `
+            void main()
+            {
+                gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+            }
+        `
+})
 
     /**
      * Points
