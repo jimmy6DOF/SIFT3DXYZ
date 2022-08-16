72fef5a0644d1c4d70bd411977008d5a93f606bd
mrmetaverse <jesse@alton.tech>
2022-08-16 16:04:55 -0400

    changed dist to docs

--- bundler/webpack.prod.js
+++ bundler/webpack.prod.js
@@ -11,7 +11,7 @@ module.exports = merge(
             new CleanWebpackPlugin()
         ],
 
-        output: {
-        clean: true,
-        }, 
+        // output: {
+        // clean: true,
+        // }, 
     })
